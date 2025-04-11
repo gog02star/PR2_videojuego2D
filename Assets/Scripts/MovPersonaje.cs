95cd62b41d7c5ae6fb9ebf40d3789e3680f1e46a
Esther Godoy <xsthxrgh@gmail.com>
2025-04-11 14:39:00 +0200

    Salto

--- Assets/Scripts/MovPersonaje.cs
+++ Assets/Scripts/MovPersonaje.cs
@@ -5,33 +5,58 @@ using UnityEngine;
 public class MovePersonaje : MonoBehaviour
 {
     public float multiplicador = 5f;
+
+    public float multiplicadorSalto = 5f;
+    private bool puedoSaltar = true;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hola Mundo!");
+        rb = this.GetComponent<Rigidbody2D>();
+
+        transform.position = new Vector3(-3.1f, -1.0f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movTeclas = Input.GetAxis("Horizontal"); //a -1f - d 1f)
-
-        
-            // aproximación 1
-/*          transform.position = new Vector3 (
-            transform.position.x+(movTeclas/100),
-            transform.position.y,
-            transgorm.position.z
-        ); */
-
         float miDeltaTime = Time.deltaTime;
-        Debug.Log(Time.deltaTime);
+        
+        //movimiento personaje
+        float movTeclas = Input.GetAxis("Horizontal"); //a -1f - d 1f)
+        float movTeclasY = Input.GetAxis("Vertical"); //a -1f - d 1f)
+        rb.velocity = new Vector2(movTeclas*multiplicador, rb.velocity.y);
+        
+        //flip
+        if(movTeclas < 0){
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if(movTeclas > 0){
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
 
-        transform.Translate(
-            movTeclas*(Time.deltaTime*multiplicador),
-            0,
-            0
-        );
+        //salto
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f);
+        Debug.DrawRay(transform.position, Vector2.down, Color.magenta);
+
+        if(hit){
+            puedoSaltar = true;
+            Debug.Log(hit.collider.name);
+            }
+        else{
+            puedoSaltar = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && puedoSaltar){ 
+            rb.AddForce(
+                new Vector2(0,multiplicadorSalto),
+                ForceMode2D.Impulse
+            );
+            //puedoSaltar = false;
+        };
+
         Debug.Log(Input.GetAxis("Horizontal"));
+       
     }
-}
+     }
+        
